4ebcdcc9c6f1a5550c644f9d33ee1397b9bfd3ab
priyaranjan-2028 <priyaranjan82@gmail.com>
2026-02-18 17:49:02 +0530

    login register successfull

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns =[
     path('', views.home, name='home'),
-    path('login/',views.login, name= 'login'),
+    path('login/',views.officer_login, name= 'login'),
     path('forget/',views.forget, name = 'forget'),
+    path('register/', views.register, name = 'register'),
+    path('Government_dashboard/', views.dashboard,name='Government_dashboard'),
 ]
