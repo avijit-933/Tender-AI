6ce756ec68e4d34644e87f345cd038b0aa849ae4
priyaranjan-2028 <priyaranjan82@gmail.com>
2026-02-18 23:01:28 +0530

    half backened successful

--- app1/urls.py
+++ app1/urls.py
@@ -7,4 +7,8 @@ urlpatterns =[
     path('forget/',views.forget, name = 'forget'),
     path('register/', views.register, name = 'register'),
     path('Government_dashboard/', views.dashboard,name='Government_dashboard'),
+    path('createtender/', views.createtender, name = 'createtender'),
+    path('evaluation/', views.evaluation, name = 'evaluation'),
+    path('managetender/', views.managetender, name = 'managetender'),
+
 ]
